0754aea50940f154162354754577fc153cee5756
nuwandev <theekshanabro40@gmail.com>
2026-04-20 18:09:52 +0530

    feat: Initialize Spring Boot project with Maven, Flyway, and PostgreSQL support

---
+++ src/main/java/com/nuwandev/reqflowapi/ReqflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuwandev.reqflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReqflowApiApplication.class, args);
+    }
+
+}
